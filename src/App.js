f708053275ddaca267bffddf6f35ebaeaaff0674
Ashutosh Kumar Singh <71124485+ashutosh44ks@users.noreply.github.com>
2022-02-06 13:01:08 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <Routes>
         <Route path="/2" element={<Page2 />} />
         <Route path="/3" element={<Page3 />} />
-        <Route path="*" element={<h1>Page Not Found!</h1>}/>
         <Route path="/" element={<Page1 />} />
       </Routes>
       <div className="button-container">
